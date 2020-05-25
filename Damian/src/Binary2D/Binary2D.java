2bb089285dd043b3ffeafc23a6311a87c5782564
damian.galazka.waw@gmail.com <damian.galazka.waw@gmail.com>
2020-05-25 15:59:38 +0200

    Fixed Binary2D - starting points properly set in compareImage

--- Damian/src/Binary2D/Binary2D.java
+++ Damian/src/Binary2D/Binary2D.java
@@ -24,7 +24,7 @@ public class Binary2D {
     private boolean compareImage(int startX, int startY){
         for(int i =0; i<pattern.widthSize;i++){
             for(int j=0; j<pattern.heightSize; j++){
-                if(mainImage.fileBits[i][j] != pattern.fileBits[i][j]) return false;
+                if(mainImage.fileBits[startX + i][startY + j] != pattern.fileBits[i][j]) return false;
             }
         }
         return true;
