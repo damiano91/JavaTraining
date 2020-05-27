52a1fe4d416f1f6f300f448e8def7a28600ea421
damian.galazka.waw@gmail.com <damian.galazka.waw@gmail.com>
2020-05-27 12:24:00 +0200

    ShortestPath - 4-if in getNextSteps() changed to 1-for loop

--- Damian/src/ShortestPath/ShortestPath.java
+++ Damian/src/ShortestPath/ShortestPath.java
@@ -25,41 +25,19 @@ public class ShortestPath {
         int i =0;
         int nextStepCount = 0;
         while(previousSteps[i] != null){
-            if (previousSteps[i].x - 1 >=0){
-                if(maze.mazeTable[previousSteps[i].x - 1][previousSteps[i].y] == 0){
-                    maze.mazeTable[previousSteps[i].x - 1][previousSteps[i].y]--;
-                    nextSteps[nextStepCount++] = new XYpoint(previousSteps[i].x - 1, previousSteps[i].y);
-                }
-                else if(maze.mazeTable[previousSteps[i].x - 1][previousSteps[i].y] == 3){
+            for(int dir = -2; dir<=2;dir++) {
+                int x = dir % 2;
+                int y = dir / 2;
+                if (x == 0 && y == 0)continue;
+                x += previousSteps[i].x;
+                y += previousSteps[i].y;
+                if ((y >= maze.heightSize) || (y < 0) ||(x >= maze.widthSize) ||(x < 0)) continue;
+                if (maze.mazeTable[x][y] == 0) {
+                    maze.mazeTable[x][y]--;
+                    nextSteps[nextStepCount++] = new XYpoint(x, y);
+                } else if (maze.mazeTable[x][y] == 3) {
                     return 1;
-                }
-            }
 
-            if (previousSteps[i].x + 1 < maze.widthSize){
-                if(maze.mazeTable[previousSteps[i].x + 1][previousSteps[i].y] == 0){
-                    maze.mazeTable[previousSteps[i].x + 1][previousSteps[i].y]--;
-                    nextSteps[nextStepCount++] = new XYpoint(previousSteps[i].x + 1, previousSteps[i].y);
-                }
-                else if(maze.mazeTable[previousSteps[i].x + 1][previousSteps[i].y] == 3){
-                    return 1;
-                }
-            }
-            if (previousSteps[i].y - 1 >=0){
-                if(maze.mazeTable[previousSteps[i].x][previousSteps[i].y - 1] == 0){
-                    maze.mazeTable[previousSteps[i].x][previousSteps[i].y - 1]--;
-                    nextSteps[nextStepCount++] = new XYpoint(previousSteps[i].x, previousSteps[i].y - 1);
-                }
-                else if(maze.mazeTable[previousSteps[i].x][previousSteps[i].y - 1] == 3){
-                    return 1;
-                }
-            }
-            if (previousSteps[i].y + 1 <maze.heightSize){
-                if(maze.mazeTable[previousSteps[i].x][previousSteps[i].y + 1] == 0){
-                    maze.mazeTable[previousSteps[i].x][previousSteps[i].y + 1]--;
-                    nextSteps[nextStepCount++] = new XYpoint(previousSteps[i].x, previousSteps[i].y + 1);
-                }
-                else if(maze.mazeTable[previousSteps[i].x][previousSteps[i].y +1 ] == 3){
-                    return 1;
                 }
             }
             i++;
