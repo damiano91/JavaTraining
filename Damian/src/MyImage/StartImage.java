202a5b66c00e2aa77a5713b4fb4b2e3b78529f45
damian.galazka.waw@gmail.com <damian.galazka.waw@gmail.com>
2020-06-05 15:10:32 +0200

    Average filter on color image

--- Damian/src/MyImage/StartImage.java
+++ Damian/src/MyImage/StartImage.java
@@ -5,23 +5,29 @@ import java.awt.*;
 
 public class StartImage {
     public static void main(String[] args) {
-        int diameter = 3;
+        int diameter = 1;
         String url = "https://cdn.wallpaperdirect.com/shared-assets/images/products/151419home.jpg";
         MyImage img = new MyImage(url);
         img.createTxt();
         img.makeGreyscale();
         img.averageFilter(diameter);
+        img.averageFilterColor(diameter);
 
         JFrame frame = new JFrame("Display image");
         JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayout(1,2));
+        mainPanel.setLayout(new GridLayout(2,2));
         ShowImage greyScale = new ShowImage(img.recreatedImg, 0, 0);
-        ShowImage greyScaleWithAverageFilter = new ShowImage(img.greysScaleWithAverageFilter, img.width, 0);
+        ShowImage greyScaleWithAverageFilter = new ShowImage(img.greysScaleWithAverageFilter, 0, 0);
+        ShowImage colorWithAverageFilter = new ShowImage((img.colorWithAverageFilter),0,0);
+        ShowImage initial = new ShowImage(img.img, 0 ,0);
         mainPanel.add(greyScale);
         mainPanel.add(greyScaleWithAverageFilter);
+        mainPanel.add(initial);
+        mainPanel.add(colorWithAverageFilter);
         frame.getContentPane().add(mainPanel);
-        frame.setSize(img.width*2,img.height);
+        frame.setSize(img.width*2,img.height*2);
         frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
     }
